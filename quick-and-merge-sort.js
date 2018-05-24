beafa765fd82654df776023080ab9090cdf7fcce
ssheaks <sarah.sheaks@gmail.com>
2018-05-24 09:33:52 -0700

    finish merge sort and add counter to quick and merge sorts

--- quick-and-merge-sort.js
+++ quick-and-merge-sort.js
@@ -4,18 +4,21 @@
 // Write a function qSort that sorts a dataset using the quicksort algorithm. The dataset to to sort is 89 30 25 32 72 70 51 42 25 24 53 55 78 50 13 40 48 32 26 2 14 33 45 72 56 44 21 88 27 68 15 62 93 98 73 28 16 46 87 28 65 38 67 16 85 63 23 69 64 91 9 70 81 27 97 82 6 88 3 7 46 13 11 64 76 31 26 38 28 13 17 69 90 1 6 7 64 43 9 73 80 98 46 27 22 87 49 83 6 39 42 51 54 84 34 53 78 40 14 5
 
 let myArray = "89 30 25 32 72 70 51 42 25 24 53 55 78 50 13 40 48 32 26 2 14 33 45 72 56 44 21 88 27 68 15 62 93 98 73 28 16 46 87 28 65 38 67 16 85 63 23 69 64 91 9 70 81 27 97 82 6 88 3 7 46 13 11 64 76 31 26 38 28 13 17 69 90 1 6 7 64 43 9 73 80 98 46 27 22 87 49 83 6 39 42 51 54 84 34 53 78 40 14 5".split(" ");
-for(let i=0; i<myArray.length; i++) { myArray[i] = +myArray[i]; }
+for(let i=0; i<myArray.length; i++) { myArray[i] = parseInt(myArray[i], 10); }
 // console.log(myArray);
 
+let qCount=0;
+
 function qSort(array, start=0, end=array.length) {
+  qCount++;
   start = start;
   end = end;
   if (start >= end) {
       return array;
   }
   const middle = partition(array, start, end);
-  array = quickSort(array, start, middle);
-  array = quickSort(array, middle + 1, end);
+  array = qSort(array, start, middle);
+  array = qSort(array, middle + 1, end);
   return array;
 };
 
@@ -38,9 +41,51 @@ function swap(array, i, j) {
   array[j] = tmp;
 };
 
-console.log(qSort(myArray));
 
 // MergeSort
 // Write a function mSort that sorts the dataset above using the mergesort algorithm.
 
 // Add functionality to both the qSort and mSort programs so that the programs shows how many operations it took to sort the same dataset
+let mCount = 0;
+function mSort(array) {
+  mCount++;
+  if (array.length <= 1) {
+      return array;
+  }
+
+  const middle = Math.floor(array.length / 2);
+  let left = array.slice(0, middle);
+  let right = array.slice(middle, array.length);
+
+  left = mSort(left);
+  right = mSort(right);
+  return merge(left, right, array);
+};
+
+function merge(left, right, array) {
+  let leftIndex = 0;
+  let rightIndex = 0;
+  let outputIndex = 0;
+  while (leftIndex < left.length && rightIndex < right.length) {
+      if (left[leftIndex] < right[rightIndex]) {
+          array[outputIndex++] = left[leftIndex++];
+      }
+      else {
+          array[outputIndex++] = right[rightIndex++];
+      }
+  }
+
+  for (let i=leftIndex; i<left.length; i++) {
+      array[outputIndex++] = left[i];
+  }
+
+  for (let i=rightIndex; i<right.length; i++) {
+      array[outputIndex++] = right[i];
+  }
+  return array;
+};
+
+console.log(mSort(myArray));
+console.log(qSort(myArray));
+console.log('merge sort', mCount);
+console.log('quick sort', qCount);
